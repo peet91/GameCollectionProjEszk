928ba6a293073d87e6b4f07cdab6f745eb707507
bdsrstnt <raymond9304@gmail.com>
2014-02-25 13:34:09 +0100

    Anyám tyúkja teszt
    Csak kipróbáltam.
    Rajmund

--- GameCollection/src/Main.java
+++ GameCollection/src/Main.java
@@ -1,6 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("That's all folks!");
+		try {
+			Scanner sc = new Scanner(new BufferedReader(new FileReader("petofi.vers")));
+			while(sc.hasNext())
+				System.out.println(sc.nextLine());
+			sc.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
